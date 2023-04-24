bcc81d15a7f771dfb0f5246eba9d4f188e6f378e
neithanmo <neithanmo@gmail.com>
2023-04-23 20:06:22 -0600

    Add support for avax purpose in bip84
    Use a third coin type which is used in avax
    
    Use avax coin type
    
    use our testing mnemonic

--- src/common/bip32.c
+++ src/common/bip32.c
@@ -186,8 +186,12 @@ int get_bip44_purpose(int address_type) {
     switch (address_type) {
         case ADDRESS_TYPE_LEGACY:
             return 44;  // legacy
+        // case ADDRESS_TYPE_WIT:
+        //     return 84;  // native segwit
+        // avax's team wants to support m/44' for 
+        // pwkh() policies
         case ADDRESS_TYPE_WIT:
-            return 84;  // native segwit
+            return 44;  // native segwit
         case ADDRESS_TYPE_SH_WIT:
             return 49;  // wrapped segwit
         case ADDRESS_TYPE_TR:
